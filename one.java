c25931f2540229bf1efbebe4612aae7ef33682c0
mkg33 <maja.k.gwozdz@gmail.com>
2018-03-06 23:44:34 +0100

    Simple integer input addition

---
+++ one.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+
+
+class one
+{
+    
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        
+        System.out.println(a+b);
+        
+        console.close();
+        System.exit(0);
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+}
